e5856858f9c13cd6c138db549181a5ae56ab7e14
Rachel Schieferstein <violenza@gmail.com>
2020-01-31 09:40:28 -0800

    adding ability to push toppings to an array in the toppings property of the pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,25 @@ function Pizza(size, toppings) {
 this.size = size;
 this.toppings = toppings;
 }
+
+Pizza.prototype.sizePrice = function() {
+  if(this.size === "s")
+}
+
+$(document).ready(function() {
+  $("form#pizzaForm").submit(function(event) {
+    event.preventDefault();
+    
+    var size = $("#size").val();
+    var toppings = []
+    var newPizza = new Pizza(size, toppings);
+
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      toppings.push(this.value);
+    });
+    
+
+    console.log(toppings)
+
+  });
+});
